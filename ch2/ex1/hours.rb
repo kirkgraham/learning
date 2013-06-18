0d69ed7af3f9a9fad2d47236bda6481af790154b
Kirk Graham <kegraham@containerstore.com>
2013-06-18 15:37:20 -0500

    Calculate the number of hours in a year.

--- ch2/ex1/hours.rb
+++ ch2/ex1/hours.rb
@@ -1,1 +1,8 @@
-puts 'find the hours in a year'
+puts "Will find the number of hours in a year"
+
+print "There are " 
+print 24 * 365
+puts " hours in a year!"
+
+puts "There are #{24 * 365} hours in a year!"
+
