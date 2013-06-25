28acf8d61decc5672efa7a96a3522eff82a9aeb8
Kirk Graham <kegraham@containerstore.com>
2013-06-25 12:14:37 -0500

    Calculate age in seconds.

---
+++ ch2/ex3/ageInSeconds.rb
@@ -0,0 +1,6 @@
+puts "What's your age? "
+age = gets.chomp.to_i
+
+puts "I am approximately #{((age * 365) * 24) * 60} seconds old."
+#puts "I am approximately #{((38 * 365) * 24) * 60} seconds old."
+
