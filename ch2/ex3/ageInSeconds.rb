d14bebc01ef83e0e6bdefa882aaadfef15fa616d
Kirk Graham <kegraham@containerstore.com>
2013-06-25 14:22:28 -0500

    Removed comment.

--- ch2/ex3/ageInSeconds.rb
+++ ch2/ex3/ageInSeconds.rb
@@ -2,5 +2,4 @@ puts "What's your age? "
 age = gets.chomp.to_i
 
 puts "I am approximately #{(((age * 60) * 60) * 24) * 365} seconds old."
-#puts "I am approximately #{((38 * 365) * 24) * 60} seconds old."
 
