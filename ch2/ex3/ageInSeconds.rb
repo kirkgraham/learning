99f18660e82d7fb24f7e20c31ce841a0d096e08c
Kirk Graham <kegraham@containerstore.com>
2013-06-25 14:21:57 -0500

    Corrected the math.

--- ch2/ex3/ageInSeconds.rb
+++ ch2/ex3/ageInSeconds.rb
@@ -1,6 +1,6 @@
 puts "What's your age? "
 age = gets.chomp.to_i
 
-puts "I am approximately #{((age * 365) * 24) * 60} seconds old."
+puts "I am approximately #{(((age * 60) * 60) * 24) * 365} seconds old."
 #puts "I am approximately #{((38 * 365) * 24) * 60} seconds old."
 
