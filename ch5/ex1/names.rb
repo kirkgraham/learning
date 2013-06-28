d13ce8ef3099bab9af3fa4bd8d47eb0cffac46a6
Kirk Graham <kegraham@containerstore.com>
2013-06-28 08:15:54 -0500

    Gather first, middle, & last names from the user then display the concatenated full name back to the user.

---
+++ ch5/ex1/names.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/ruby
+
+puts "Please enter your first name: "
+first_name = gets.chomp
+
+puts "Please enter your middle name: "
+middle_name = gets.chomp
+
+puts "Please enter your last name: "
+last_name = gets.chomp
+
+puts "Your full name is " + first_name + ' ' + middle_name + ' ' + last_name
+
