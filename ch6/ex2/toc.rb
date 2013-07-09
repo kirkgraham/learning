9ddcd0dc5960ed8cf266236ee896cb12fbe810c2
Kirk Graham <kegraham@containerstore.com>
2013-07-09 12:03:05 -0500

    Print out a properly formatted table of contents.

---
+++ ch6/ex2/toc.rb
@@ -0,0 +1,14 @@
+screen_width = 100
+puts "Table of Contents".center(screen_width)
+
+puts ""
+
+print "Chapter 1:  Getting Started".ljust(screen_width / 2)
+puts "page  1".rjust(screen_width / 2)
+
+print "Chapter 2:  Numbers".ljust(screen_width / 2)
+puts "page  9".rjust(screen_width / 2)
+
+print "Chapter 3:  Letters".ljust(screen_width / 2)
+puts "page 13".rjust(screen_width / 2)
+
